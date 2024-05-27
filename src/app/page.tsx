fcb9378924a99965d14b1c1664c490766f5faa4b
Caleb Forestal | <133164966+Calebrlx@users.noreply.github.com>
2024-05-27 12:49:34 -0800

    f

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+// src/app/page.tsx
+
+const HomePage = () => {
+    return (
+      <div>
+        <h1>Welcome to My Simple Next.js App!</h1>
+        <p>This is a simple Next.js application with app routing.</p>
+      </div>
+    );
+  };
+  
+  export default HomePage;
